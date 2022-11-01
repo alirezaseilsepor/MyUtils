fdb9d58c7100c93c496957a2c48100ce9d91b36d
Alireza72 <alirezaseilsepor@gmail.com>
2022-11-01 12:45:25 +0330

    add ktx getPrettyFormatForNumbers

--- mylibrary/src/main/java/app/king/mylibrary/ktx/Long.kt
+++ mylibrary/src/main/java/app/king/mylibrary/ktx/Long.kt
@@ -2,6 +2,7 @@ package app.king.mylibrary.ktx
 
 import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.math.abs
 
 fun Long.toDigitalClockTime(showHour: Boolean = true): String {
     val milliSeconds = this
@@ -17,4 +18,36 @@ fun Long.toDigitalClockTime(showHour: Boolean = true): String {
             "${prefixHour}00:00"
         }
     }
+}
+
+fun Long?.getPrettyFormatForNumbers(): String {
+    if (this == null)
+        return "0"
+    val x = abs(this.toDouble())
+    var label = ""
+    val value: Double
+    val thousand = 1000.0
+    val million = 1000000.0
+    val billion = 1000000000.0
+    when {
+        x < thousand -> value = x
+        x < million -> {
+            value = x / thousand
+            label = " K"
+        }
+        x < billion -> {
+            value = x / million
+            label = " M"
+        }
+        else -> {
+            value = x / billion
+            label = " B"
+        }
+    }
+    var result = "$value$label"
+    val split = "$value".split(".")
+    if (split[1].trim().toInt() == 0) {
+        result = result.replace(".0", "")
+    }
+    return result
 }
