fe171b8d8ee90e81e1fbe4a6044d09253fc3a9a4
Alireza72 <alirezaseilsepor@gmail.com>
2022-11-04 10:19:52 +0330

    improve

--- mylibrary/src/main/java/app/king/mylibrary/ktx/TextView.kt
+++ mylibrary/src/main/java/app/king/mylibrary/ktx/TextView.kt
@@ -6,6 +6,7 @@ import android.widget.TextView
 
 fun TextView.setAppearance(res: Int) {
     if (Build.VERSION.SDK_INT < 23) {
+        @Suppress("DEPRECATION")
         setTextAppearance(this.context, res)
     } else {
         setTextAppearance(res)
